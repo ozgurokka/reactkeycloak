9398f42b2a06a064c3b470120db8265648e095f3
ozgur <ozgur>
2022-10-25 09:41:03 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
+import Nav from "./components/Nav";
+import WelcomePage from "./pages/Homepage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SecuredPage from "./pages/Securedpage";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./Keycloak"
+import PrivateRoute from "./helpers/PrivateRoute";
+
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Nav />
+       <BrowserRouter>
+         <Routes>
+           <Route exact path="/" element={<WelcomePage />} />
+           <Route
+             path="/secured"
+             element={
+               <PrivateRoute>
+                 <SecuredPage />
+               </PrivateRoute>
+             }
+           />
+         </Routes>
+       </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./Keycloak"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
+  
+<ReactKeycloakProvider authClient={keycloak}>
+<React.StrictMode>
     <App />
   </React.StrictMode>
+</ReactKeycloakProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
